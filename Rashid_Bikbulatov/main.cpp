d710a201e9d988c5b69bbc6a9587a8de2fd8ca89
Rasha108 <rasha1995bik@gmail.com>
2020-12-02 21:08:49 +0300

    add struct and func checDiagonal

--- Rashid_Bikbulatov/main.cpp
+++ Rashid_Bikbulatov/main.cpp
@@ -2,14 +2,13 @@
 #include <string>
 #include <vector>
 #include <map>
+#include "array"
 
 using namespace std;
 
 class Solution {
 public:
-    Solution() = default;
-
-    string tictactoe(vector<vector<int>>& moves) {
+    string tictactoe(const vector<vector<int>>& moves) {
         for (size_t i = 0; i < moves.size(); ++i) {
             string tic;
             if (i % 2 == 0) {
@@ -18,22 +17,23 @@ public:
                 tic = '0';
             }
 
-            grid[tic].push_back(moves[i][0]);
-            grid[tic].push_back(moves[i][1]);
+            vector<int> move = moves[i];
+            grid[tic].push_back(move.front());
+            grid[tic].push_back(move.back());
         }
 
-        // check win player A
-        if (checkPlayerWin("X")) {
-            return "A";
+        Player_A playerA;
+        if (checkPlayerWin(playerA.char_move)) {
+            return playerA.player;
         }
 
-        // check win player B
-        if (checkPlayerWin("0")) {
-            return "B";
+        Player_B playerB;
+        if (checkPlayerWin(playerB.char_move)) {
+            return playerB.player;
         }
 
         // check end move
-        if (moves.size() == 9u) {
+        if (moves.size() == max_moves) {
             return "Draw";
         }
 
@@ -42,9 +42,41 @@ public:
 
 private:
     map<string, vector<int>> grid;
+    const size_t max_moves = 9;
+
+    const struct {
+        string player;
+        string char_move;
+    } Player_A = {"A", "X"};
+
+    const struct {
+        string player;
+        string char_move;
+    } Player_B = {"B", "0"};
+
+    bool checkDiagonal(const array<array<int, 3>, 3>& arr) {
+        if (arr[1][1] == 0) {
+            return false;
+    }
+
+        return ((arr[0][0] && arr[2][2] > 0) ||
+                (arr[0][2] && arr[2][0] > 0));
+    }
+
+    bool checkRowsAndCols(const array<array<int, 3>, 3>& arr) {
+        if ((arr[0][0] && arr[0][1] && arr[0][2] == 1) || (arr[0][0] && arr[1][0] && arr[2][0] == 1) ) {
+            return true;
+        } else if (arr[1][0] && arr[1][1] && arr[1][2] == 1 || (arr[0][1] && arr[1][1] && arr[2][1] == 1) ) {
+            return true;
+        } else if (arr[2][0] && arr[2][1] && arr[2][2] == 1 || (arr[0][2] && arr[1][2] && arr[2][2] == 1) ) {
+            return true;
+        }
+
+        return false;
+    }
 
     bool checkPlayerWin(const string& player) {
-        int array[3][3] = {0};
+        array<array<int, 3>, 3> arr{};
         bool check = false;
 
         check = false;
@@ -52,35 +84,16 @@ private:
         for (const auto& i : grid[player]) {
             // iteration through +2
             if (check) {
-                array[row][i] = 1;
+                arr[row][i] = 1;
                 check = false;
                 continue;
             }
-            if (i == 0) {
-                row = i;
-            } else if (i == 1) {
-                row = i;
-            } else if (i == 2) {
-                row = i;
-            }
+
+            row = i;
             check = true;
         }
 
-        if ((array[0][0] && array[0][1] && array[0][2] == 1) || (array[0][0] && array[1][0] && array[2][0] == 1) ) {
-            return true;
-        } else if (array[1][0] && array[1][1] && array[1][2] == 1 || (array[0][1] && array[1][1] && array[2][1] == 1) ) {
-            return true;
-        } else if (array[2][0] && array[2][1] && array[2][2] == 1 || (array[0][2] && array[1][2] && array[2][2] == 1) ) {
-            return true;
-        }
-
-        // check diagonal
-        if (array[1][1] == 0) {
-            return false;
-        }
-
-        return ((array[0][0] && array[2][2] > 0) ||
-                (array[0][2] && array[2][0] > 0));
+        return checkRowsAndCols(arr) || checkDiagonal(arr);
     }
 };
 
@@ -89,8 +102,8 @@ int main() {
     Solution solution;
     vector<vector<int>> moves = {{0,0}, {1, 1}, {2, 0}, {1, 0}, {1, 2}, {2, 1}, {0, 1}, {0, 2}, {2, 2}};
 
-    string tic = solution.tictactoe(moves);
-    cout << tic << endl;
+    string result = solution.tictactoe(moves);
+    cout << result << endl;
 
     return 0;
 }
